31b6a1fbc86a32b6249b01cd367435f8ae9fc4be
khuzaemah <khuza.emah24@gmail.com>
2022-10-11 12:42:52 +0700

    add feature paralax jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
-// Paralax
+// About
+// $(window).on('load', function () {
+//     $('.pLeft').addClass('pShow');
+//     $('.pRight').addClass('pShow');
+// });
+
+
+// Parallax
 $(window).scroll(function () {
 
     let wScroll = $(this).scrollTop();
     // console.log(wScroll);
 
+    // About
+    if (wScroll > $('#about').offset().top - 300) {
+        $('.pLeft').addClass('pShow');
+        $('.pRight').addClass('pShow');
+    }
+
+    // Jumbotron
     $('.jumbroton img').css({
         'transform': 'translate(0px, ' + wScroll / 4 + '%)'
     });
@@ -15,5 +29,16 @@ $(window).scroll(function () {
     $('.jumbroton p').css({
         'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
     });
+
+
+    // Project
+    if (wScroll > $('#project').offset().top - 250) {
+        $('#project .card').each(function (i) {
+            setTimeout(function () {
+                $('#project .card').eq(i).addClass('show');
+            }, 200 * (i + 1))
+        });
+
+    }
 
 });
